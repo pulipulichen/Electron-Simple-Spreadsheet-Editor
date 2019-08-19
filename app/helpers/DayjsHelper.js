38e9429835422092a9721e043f5d9538620eafd1
Pulipuli Chen <blog@pulipuli.info>
2019-08-19 23:53:41 +0800

    0819 close #42 #36 好，基本的完成

--- app/helpers/DayjsHelper.js
+++ app/helpers/DayjsHelper.js
@@ -3,6 +3,17 @@
 let DayjsHelper = {
   init: function () {
     dayjs.extend(dayjs_plugin_relativeTime)
+  },
+  getTimestamp: function () {
+    return (new Date()).getTime()
+  },
+  /**
+   * 
+   * @param {type} timestamp = (new Date()).getTime()
+   * @returns {unresolved}
+   */
+  howLongAgo: function (timestamp) {
+    return dayjs().from(timestamp) // 2 years ago
   }
 }
 
