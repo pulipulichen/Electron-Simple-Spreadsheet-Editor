b6c24ed9c71dd41753c0e798178f906c3b6ec250
Pulipuli Chen <blog@pulipuli.info>
2019-08-24 17:35:02 +0800

    0819 #46 算了我放棄，移除吧

--- w-renderer.js
+++ w-renderer.js
@@ -1,8 +1,13 @@
 // renderer.js
 // remote用于在主进程和渲染进程之间建立联系
 // clipboard可以提供复制文本的功能
-import {clipboard, remote} from 'electron';
+const electron = require('electron')
+const remote = electron.remote
+//import {clipboard, remote} from 'electron';
+//require = window.require
 
 // 例如，你可以用remote获取当前的窗口
 let currentWindow = remote.getCurrentWindow();
 console.log(currentWindow); // It's an Object
+
+console.log('Hello, world.')
